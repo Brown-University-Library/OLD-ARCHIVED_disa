70721d5378302dd47c30e5208b67f504a1f9f810
Steven McCauley <steven_mccauley@brown.edu>
2018-05-18 16:39:30 -0400

    Get Add Document process working (sorta)

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField, SelectField, DateField
 
 class DocumentForm(FlaskForm):
-    name = StringField('Display name')
+    # From MongoDB document.sourceType field
+    doctype = SelectField('Type', choices=[('news','Newspaper'),
+        ('ltr','Letter'),('rgst','Registry'),('invt','Inventory'),
+        ('cen','Census'),('court','Court Document'),
+        ('prob','Probate Account'),('will','Will')])
+    citation = StringField('Citation information')
+    date = DateField('Date of publication')
+    context = StringField('National context')
+    zotero = StringField('Zotero ID')
+    comments = TextAreaField('Comments')
+    submit = SubmitField('Create')
+
+class RecordForm(FlaskForm):
+    # From MongoDB document.recordType field
+    rectype = SelectField('Type', choices=['Manumission',
+        'Runaway advertisement','Advertisement of Sale',
+        'Baptism', 'Runaway capture advertisement',
+        'Smallpox inoculation notice', 'Execution notice'])
     date = StringField('Date of publication')
     context = StringField('National context')
     citation = StringField('Citation information')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import request, jsonify, render_template
+from flask import request, jsonify, render_template, redirect, url_for
 from app import app, db
-from app.models import Document
+from app.models import Document, Record
 from app.forms import DocumentForm
 
 @app.route('/')
@@ -12,7 +12,26 @@ def documents_index():
     all_docs = Document.query.all()
     return render_template('document_index.html', documents=all_docs)
 
+@app.route('/documents/<docId>', methods=['GET'])
+def get_document(docId):
+    doc = Document.query.filter_by(id=docId).first_or_404()
+    return render_template('document.html', document=doc)
+
 @app.route('/documents/new', methods=['GET','POST'])
 def new_document():
     form = DocumentForm()
+    # if form.validate_on_submit():
+    if request.method == 'POST':
+        doc = Document(doctype=form.doctype.data,
+            date=form.date.data, national_context=form.context.data,
+            citation=form.citation.data, zotero_id=form.zotero.data,
+            comments=form.comments.data)
+        db.session.add(doc)
+        db.session.commit()
+        return redirect(url_for('get_document', docId=doc.id))
     return render_template('new_document.html', form=form)
+
+@app.route('/records/<recId>', methods=['GET'])
+def get_record(recId):
+    rec = Record.query.filter_by(id=recId).first_or_404()
+    return render_template('record.html', record=rec)
