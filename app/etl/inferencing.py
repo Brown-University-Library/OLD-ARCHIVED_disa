34ba24ba4b0bd843b71824bec2a3332fe0fd6233
Steven McCauley <steven_mccauley@brown.edu>
2018-07-12 15:23:46 -0400

    Converge Persons

--- app/etl/inferencing.py
+++ app/etl/inferencing.py
@@ -6,6 +6,34 @@ def extract_information():
     for rec in records:
         converge_record_names(rec)
 
+    docs = models.Document.query.all()
+    for doc in docs:
+        converge_document_people(doc)
+
+def converge_persons(peopleList):
+    preserve = peopleList[0]
+    for rmv in peopleList[1:]:
+        preserve.references.extend(rmv.references)
+    db.session.add(preserve)
+    db.session.commit()
+    for rmv in peopleList[1:]:
+        db.session.delete(rmv)
+    db.session.commit()
+
+def converge_document_people(doc):
+    to_converge = defaultdict(list)
+    for rec in doc.records:
+        for e in rec.entrants:
+            person = e.person
+            formatted_name = '{} {}'.format(
+                person.first_name.strip(), person.last_name.strip()).strip()
+            to_converge[ formatted_name ].append(person)
+    for c in to_converge:
+        if len(to_converge[c]) > 1:
+            if c != '' and c != 'John Brashaw':
+                print('Converging {} for document {}'.format(c, doc.id))
+                converge_persons(to_converge[c])
+
 def converge_entrants(entrantList):
     preserve = entrantList[0]
     for rmv in entrantList[1:]:
@@ -23,7 +51,8 @@ def converge_entrants(entrantList):
 def converge_record_names(record):
     to_converge = defaultdict(list)
     for e in record.entrants:
-        formatted_name = '{} {}'.format(e.first_name, e.last_name).strip()
+        formatted_name = '{} {}'.format(
+            e.first_name.strip(), e.last_name.strip()).strip()
         to_converge[ formatted_name ].append(e)
     for c in to_converge:
         if len(to_converge[c]) > 1:
