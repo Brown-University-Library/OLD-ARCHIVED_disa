de11ef69ceea1c5742f3037ee5825984818c4b11
Steven McCauley <steven_mccauley@brown.edu>
2019-10-10 14:56:15 -0400

    Refactor Source

--- app/static/js/editor/flowcontrol.js
+++ app/static/js/editor/flowcontrol.js
@@ -32,3 +32,54 @@ class Config {
     return this._data[attr];
   }
 }
+
+class Source {
+
+  constructor($endpoints, $csrfToken) {
+    this._app = {};
+    this.endpoints = this.getEndpoints($endpoints);
+    this._csrf = $csrfToken.attr('data-csrf');
+
+    $.ajaxSetup({
+      beforeSend: function(xhr, settings) {
+        xhr.setRequestHeader("Access-Control-Allow-Origin", "http://0.0.0.0:5000");
+
+        if (RegExp('^(POST|PUT|PATCH|DELETE)$').test(settings.type) && !this.crossDomain) {
+          xhr.setRequestHeader("X-CSRFToken", this._csrf);
+        }
+      }
+    });
+  }
+
+  setApp( app ) {
+    this._app = app;
+  }
+
+  getEndpoints( $endpoints ) {
+    let endpoint_map = {};
+
+    $endpoints.find('.endpoint').each(function() {
+      endpoint_map[$(this).attr('data-name')] = $(this).attr('data-url');
+    });
+
+    return endpoint_map;
+  }
+
+  request( endpoint, method, callback, payload ) {
+    let settings = {
+      type: method,
+      dataType: "json",      
+      url: endpoint,
+      context: this,
+      success: function( data ) {
+        this._app[callback]( data );
+      }
+    };
+
+    if (method === 'POST' || method === 'PUT') {
+      settings.contentType = "application/json";
+      settings.data = JSON.stringify(payload);
+    }
+    $.ajax(settings);
+  }
+}
