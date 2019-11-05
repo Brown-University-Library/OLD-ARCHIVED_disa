4f4984e6deda63101c008a54d73df700715645a0
birkin <birkin.diana@gmail.com>
2019-11-05 11:42:16 -0500

    removes print statements.

--- app/__init__.py
+++ app/__init__.py
@@ -11,17 +11,13 @@ import shellvars
 
 
 app = Flask(__name__)
-print( '__init__.py loaded' )
-
-## config
-print( f'initial environ, ```{pprint.pformat(os.environ.__dict__)}```' )
+# print( '__init__.py loaded' )
 
 
 ## load up env vars
 envar_dct = shellvars.get_vars( os.environ['DISA_FL__SETTINGS_PATH'] )
 for ( key, val ) in envar_dct.items():
     os.environ[key.decode('utf-8')] = val.decode('utf-8')
-
 
 try:
     app.config.from_object(os.environ['APP_SETTINGS'])  # loads env from `dotenv` module
