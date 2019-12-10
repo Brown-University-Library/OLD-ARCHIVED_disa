2616806939354173144a0beaba8cbfcc570fe660
birkin <birkin.diana@gmail.com>
2019-12-10 11:16:18 -0500

    adds logging to troubleshoot phantom-date issue.

--- app/routes.py
+++ app/routes.py
@@ -59,33 +59,55 @@ def logout():
     logout_user()
     return redirect(url_for('browse'))
 
+# def sort_documents(wrappedDocs):
+#     merge = {}
+#     for w in wrappedDocs:
+#         if w[0].id not in merge or merge[w[0].id][0] < w[2]:
+#             merge[w[0].id] = (w[2], w[3], w[0])
+#         else:
+#             continue
+#     return sorted([ merge[w] for w in merge], reverse=True)
+
 def sort_documents(wrappedDocs):
+    log.debug( f'before sort (first 10), ```{pprint.pformat(wrappedDocs[0:10])}...```' )
     merge = {}
     for w in wrappedDocs:
         if w[0].id not in merge or merge[w[0].id][0] < w[2]:
             merge[w[0].id] = (w[2], w[3], w[0])
         else:
             continue
-    return sorted([ merge[w] for w in merge], reverse=True)
+    sorted_docs = sorted([ merge[w] for w in merge], reverse=True)
+    # log.debug( f'sorted_docs (first 10), ```{pprint.pformat(sorted_docs)}```...' )
+    return sorted_docs
 
 @app.route('/editor', methods=['GET'])
 @login_required
 def editor_index():
-    log.debug( 'starting editor_index' )
+    log.debug( '\n\nstarting editor_index' )
     all_cites = models.Citation.query.all()
+    log.debug( f'all_cites (first 10), ```{pprint.pformat(all_cites[0:10])}...```' )
     no_refs = [ (cite, current_user.id, datetime.datetime.now(), '')
         for cite in all_cites if len(cite.references) == 0 ]
+    log.debug( f'no_refs (first 10), ```{pprint.pformat(no_refs[0:10])}...```' )
     has_refs = [ cite for cite in all_cites if len(cite.references) > 0 ]
+    log.debug( f'has_refs (first 10), ```{pprint.pformat(has_refs[0:10])}...```' )
     wrapped_refs = [ (cite, edit.user_id, edit.timestamp, edit.edited_by.email)
                         for cite in has_refs
                             for ref in cite.references
                                 for edit in ref.edits ]
+    log.debug( f'wrapped_refs (first 20), ```{pprint.pformat(wrapped_refs[0:20])}...```' )
     user_cites = [ wrapped for wrapped in wrapped_refs
                     if wrapped[1] == current_user.id ]
+    log.debug( f'user_cites (first 10), ```{pprint.pformat(user_cites[0:10])}...```' )
     srtd_all = sort_documents(no_refs + wrapped_refs)
+    log.debug( f'srtd_all (first 10), ```{pprint.pformat(srtd_all[0:10])}...```' )
     srtd_user = sort_documents(user_cites)
-    return render_template('document_index.html',
-        user_documents=srtd_user, documents=srtd_all)
+    log.debug( f'srtd_user (first 10), ```{pprint.pformat(srtd_user[0:10])}...```' )
+    return render_template(
+        'document_index.html',
+        user_documents=srtd_user,
+        documents=srtd_all
+        )
 
 @app.route('/editor/documents')
 @app.route('/editor/documents/<citeId>')
